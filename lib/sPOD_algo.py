Add the TV regularization on the V factor in BFB (BFBTV_v2) and fix the partial derivatives in the chain rule for S term

lib/sPOD_algo.py
@@ -15,7 +15,7 @@ import time
 from timeit import default_timer as timer
 
 import numpy as np
-from scipy.linalg import sqrtm
+from scipy.linalg import sqrtm, khatri_rao
 from numpy import reshape
 from numpy.linalg import norm
 from dataclasses import dataclass
@@ -469,9 +469,9 @@ def shifted_POD_BFBTV_v2(
     N_U = myparams.tv_niter
     N_S = myparams.tv_niter
     N_V = myparams.tv_niter
-    step_U = 1 / Nframes
-    step_S = 1 / Nframes
-    step_V_prim = 1 / Nframes
+    step_U = 0.1 / Nframes
+    step_S = 0.1 / Nframes
+    step_V_prim = 0.1 / Nframes
     step_V_dual = 0.99 / (step_V_prim*spnorm_D**2)
     
     while abs(rel_decrease) > myparams.eps and current_it < myparams.maxit:
@@ -505,16 +505,16 @@ def shifted_POD_BFBTV_v2(
             # 1) Update in U^k (projected gradient descent)
             for subiter in range(N_U):
                 # Gradient step
-                G_U = res_shifted*S@VT
+                G_U = res_shifted*S@VT.T
                 U = U + step_U * G_U
                 # Projection step
                 U = U @ np.linalg.inv(sqrtm(U.T@U))
             # 2) Update in s^k (proximal gradient descent)
             for subiter in range(N_S):
-                G_S = res_shifted.T@U@VT
+                G_S = khatri_rao(VT.T,U).T@res_shifted.flatten()
+                #G_S = res_shifted.T@U@VT
                 S = shrink(S + step_S * G_S, myparams.lambda_s*step_S)
-                S = np.diag(S)
-            # 3) Update in V^k
+            # 3) Update in V^k (RFBPD)
             for subiter in range(N_V):
                 # 3.a) Primal update
                 G_V = res_shifted.T@U*S